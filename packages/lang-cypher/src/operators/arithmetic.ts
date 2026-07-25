a8181f38878f016f5b6c436534ae567ed722850d
Filip Ježek <filip.jezek@gmail.com>
2026-07-26 01:00:36 +0200

    fix: shortcut nulls in cypher "+" operator

--- packages/lang-cypher/src/operators/arithmetic.ts
+++ packages/lang-cypher/src/operators/arithmetic.ts
@@ -1,11 +1,12 @@
 import { Operator } from '@dortdb/core';
+import { shortcutNulls } from '@dortdb/core/utils';
 
 export const add: Operator = {
   name: '+',
-  impl: (a: any, b: any) => {
+  impl: shortcutNulls((a: any, b: any) => {
     if (Array.isArray(a) && Array.isArray(b)) {
       return [...a, ...b];
     }
     return a + b;
-  },
+  }),
 };
