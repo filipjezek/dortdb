2d297fece152db20a76bd190f5885000102e77b1
tuaki <jachymb32@gmail.com>
2026-08-03 23:24:52 +0200

    stuff

--- packages/testing/benchmarks/README.md
+++ packages/testing/benchmarks/README.md
@@ -8,27 +8,7 @@ npm install
 nx build benchmarks
 cd packages/testing/benchmarks
 ```
-
-Run:
-```bash
-npm run benchmark -- -b tpch -d dortdb -q 1
-npm run benchmark -- -b tpch -d sqlite -q 1
-npm run benchmark -- -b tpch -d alasql -q 1
-
-npm run benchmark -- -b unibench -d dortdb -q 1
-npm run benchmark -- -b unibench -d orient -q 1
-npm run benchmark -- -b unibench -d arango -q 1
-
-# Full data
-# npm run benchmark -- -b unibench -u https://github.com/HY-UDBMS/UniBench/releases/download/0.2/Unibench-0.2.zip -d dortdb -q 1
-# npm run benchmark -- -b unibench -u https://github.com/HY-UDBMS/UniBench/releases/download/0.2/Unibench-0.2.zip -d orient -q 1
-# npm run benchmark -- -b unibench -u https://github.com/HY-UDBMS/UniBench/releases/download/0.2/Unibench-0.2.zip -d arango -q 1
-
-# npm run unibench
-# npm run tpch
-# npm run tpch:sqlite
-# npm run tpch:alasql
-```
+(If you don't want to install Nx, `npx nx build benchmarks` should also work.)
 
 ### Experiments
 
@@ -40,3 +20,7 @@ python -m pip install -r requirements.txt
 ```
 
 In VS Code, select the Python interpreter for the experiments folder. Press `Ctrl+Shift+P` and search for "Python: Select Interpreter", press "Enter interpreter path" and paste `packages/testing/benchmarks/experiments/.venv/bin/python`.
+
+```bash
+python -m measure_queries
+```
--- packages/testing/benchmarks/experiments/plot_results.py
+++ packages/testing/benchmarks/experiments/plot_results.py
@@ -7,29 +7,32 @@ from common import DORTDB_RULES, OUTPUT_DIR, QUERY_TIMEOUT_MS, Benchmark
 
 def main():
     _plot_tpch()
-    _plot_unibench()
     _plot_unisample()
-    _plot_tpch_rules()
+    _plot_unibench()
+    # _plot_tpch_rules()
 
 def _plot_tpch():
     results = Benchmark('tpch').load_results()
     databases = ['dortdb', 'alasql', 'sqlite']
-    plot_benchmark_query_times(results, databases, 'bar', fillna=True, save_as='tpch_medians.pdf')
-
-def _plot_unibench():
-    results = Benchmark('unibench').load_results()
-    databases = ['dortdb', 'arango', 'orient']
-    plot_benchmark_query_times(results, databases, 'bar', fillna=True, save_as='unibench_medians.pdf')
+    labels=['DortDB', 'AlaSQL', 'SQL.js']
+    plot_benchmark_query_times(results, databases, 'bar', (12, 4), 'TPC-H Query', save_as='tpch_medians.pdf', labels=labels)
 
 def _plot_unisample():
     results = Benchmark('unisample').load_results()
     databases = ['dortdb', 'arango']
-    plot_benchmark_query_times(results, databases, 'bar', fillna=True, save_as='unisample_medians.pdf', legend_loc='upper right')
+    labels = ['DortDB', 'ArangoDB']
+    plot_benchmark_query_times(results, databases, 'bar', (8, 4), 'UniBench Sample Query', save_as='unisample_medians.pdf', labels=labels)
+
+def _plot_unibench():
+    results = Benchmark('unibench').load_results()
+    databases = ['dortdb', 'arango', 'orient']
+    labels = ['DortDB', 'ArangoDB', 'OrientDB']
+    plot_query_times(results, databases, 1, (6, 3), 'UniBench Dataset, Query 1', save_as='unibench_medians.pdf', labels=labels)
 
 def _plot_tpch_rules():
     results = Benchmark('tpch').load_results()
-    databases = [f'dortdb_exclude_{rule}' for rule in DORTDB_RULES] + ['dortdb_exclude_all']
-    plot_benchmark_query_times(results, databases, 'bar', fillna=True, save_as='tpch_rules_medians.pdf', legend_loc='upper right')
+    databases = [f'dortdb_exclude_{rule}' for rule in DORTDB_RULES]
+    plot_benchmark_query_times(results, databases, 'bar', (16, 6), 'TPC-H Query', save_as='tpch_rules_medians.pdf', legend_loc='upper right')
 
 def plot_database_query_times(results: pd.DataFrame, database: str):
     """Plot all query execution times for a given benchmark and database.
@@ -42,9 +45,13 @@ def plot_database_query_times(results: pd.DataFrame, database: str):
         row = group.iloc[0]
         times = row['runs']
         label = f'{database}-q{query_id}'
-        _plot_query_times(label, times)
+        if len(times) == 0:
+            print(f'No runs for {label}. Skipping.')
+            continue
 
-def _plot_query_times(query_label: str, times: np.ndarray):
+        _plot_query_times_variance(label, times)
+
+def _plot_query_times_variance(query_label: str, times: np.ndarray):
     max = np.max(times)
     normalized_times = times / max  # Normalize the times for better visualization
     std = np.std(normalized_times)
@@ -61,9 +68,13 @@ def plot_benchmark_query_times(
         results: pd.DataFrame,
         databases: list[str],
         style: str,
-        fillna: bool,
+        size: tuple[float, float],
+        x_label: str,
+        fillna = True,
         save_as: str | None = None,
-        legend_loc: LegendLocType | None = None
+        labels: list[str] | None = None,
+        legend_loc: LegendLocType | None = None,
+        legend_max_ncols = 3
     ):
     """Plots the median execution times of queries (with std error bars) for a given benchmark and databases."""
     df = (
@@ -80,7 +91,7 @@ def plot_benchmark_query_times(
     # Remove queries that have NaN for all databases.
     df = df[~df.isna().all(axis=1)]
 
-    plt.figure(figsize=(20, 6))
+    plt.figure(figsize=size)
     ax = plt.gca()
 
     x_ticks = df.index
@@ -104,9 +115,10 @@ def plot_benchmark_query_times(
         error_coefficient = np.sqrt(np.pi / 2)
         errors = df[(db, 'std')] * error_coefficient
 
+        label = labels[db_index] if labels is not None else db
+
         if style == 'bar':
             bar_width = 0.75 / len(databases)
-
             color = COLORS[db_index]
             colors = ['white' if is_nan else color for is_nan in nan_map]
             hatches = ['//' if is_nan else None for is_nan in nan_map]
@@ -116,7 +128,7 @@ def plot_benchmark_query_times(
                 medians,
                 bar_width,
                 yerr=errors,
-                label=db,
+                label=label,
                 capsize=4,
                 color=colors,
                 edgecolor=color,
@@ -130,22 +142,83 @@ def plot_benchmark_query_times(
                 # yerr=errors,
                 marker='.',
                 # linestyle='None',
-                label=db,
+                label=label,
                 zorder=4,
             )
 
     ax.set_ylabel('Median execution time (ms)')
-    ax.set_xlabel('TPC-H Query')
+    ax.set_xlabel(x_label)
 
     loc = legend_loc if legend_loc is not None else 'upper left'
     ncols = len(databases)
-    if ncols > 3:
-        ncols = 3
+    if ncols > legend_max_ncols:
+        ncols = legend_max_ncols
     ax.legend(loc=loc, ncols=ncols)
 
     x_vlines = [x - 0.5 for x in x_ticks[1:]]
     # get the current maximal y value:
     plt.vlines(x_vlines, ymin=0, ymax=y_max, colors='0.8', linewidth=0.5)
+    plt.tight_layout()
+
+    if save_as:
+        path = OUTPUT_DIR / save_as
+        plt.savefig(path, bbox_inches='tight')
+    else:
+        plt.show()
+
+def plot_query_times(
+        results: pd.DataFrame,
+        databases: list[str],
+        query_id: int,
+        size: tuple[float, float],
+        title: str,
+        fillna = True,
+        save_as: str | None = None,
+        labels: list[str] | None = None,
+    ):
+    """Plots the median execution times of queries (with std error bars) for a given benchmark and databases."""
+    df = (
+        results[results['queryId'] == query_id]
+            .reset_index()
+            [['database', 'median', 'std']]
+    )
+
+    plt.figure(figsize=size)
+    ax = plt.gca()
+
+    x_ticks = df.index
+    ax.set_xticks(x_ticks)
+    ax.set_xticklabels(labels if labels is not None else df['database'])
+
+    ax.set_yscale('log')
+    ax.grid(which='both', color='0.9', axis='y')
+
+    medians = df['median']
+    nan_map = [np.isnan(median) for median in medians]
+    if fillna:
+        medians = medians.fillna(QUERY_TIMEOUT_MS)
+
+    # Standard error of the median is approximated as std * sqrt(pi / 2) (we assume normal distribution of the data).
+    error_coefficient = np.sqrt(np.pi / 2)
+    errors = df['std'] * error_coefficient
+
+    colors = ['white' if is_nan else COLORS[i] for i, is_nan in enumerate(nan_map)]
+    edgecolors = [COLORS[i] for i in range(len(nan_map))]
+    hatches = ['//' if is_nan else None for is_nan in nan_map]
+
+    ax.bar(
+        x_ticks,
+        df['median'],
+        yerr=errors,
+        capsize=4,
+        color=colors,
+        edgecolor=edgecolors,
+        hatch=hatches,
+        zorder=4,
+    )
+
+    ax.set_ylabel('Median execution time (ms)')
+    plt.title(title)
     plt.tight_layout()
 
     if save_as:
